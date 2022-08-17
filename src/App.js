c5bdc2577eb2b38a442e358fc1254c62e5d75b18
ignacio <ignaciomarucco@gmail.com>
2022-08-17 14:43:48 -0300

    Add ItemListContainer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import {NavBar} from './components/NavBar/NavBar.js'
+import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer.js";
 
 function App() {
   return (
     <>
       <NavBar/>
+      <ItemListContainer greeting="Bienvenido a JC Store"/>
     </>
   );
 }
