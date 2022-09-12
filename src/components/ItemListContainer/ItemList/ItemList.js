3c408811fd2556fbe066b41885855c87cf3142dc
ignacio <ignaciomarucco@gmail.com>
2022-09-12 17:20:56 -0300

    Clean code

--- src/components/ItemListContainer/ItemList/ItemList.js
+++ src/components/ItemListContainer/ItemList/ItemList.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import {Item} from '../Item/Item.js';
 
-import {Link} from 'react-router-dom';
-
 export const ItemList = ({items}) => {
   return (
     <div className='row row-cols-auto justify-content-center container-fluid'>
       {
         items.map((item) => (
-          // <Link key={item.id} to={`/item/${item.id}`}>
-          //   <Item item = {item}/> 
-          // </Link>
           <Item key={item.id} item = {item}/> 
         ))
       }
